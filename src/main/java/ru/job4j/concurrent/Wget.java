cc4988cedeaa0a413d92ccc9833c38c07d11c1c4
Mouzzli <cokes03@yandex.ru>
2022-09-17 01:58:54 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,14 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            try {
+                System.out.print("\rLoading: " + i + "%");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
