5a12ada5361399b78122a37f9020b4d6aba8aba8
Mouzzli <cokes03@yandex.ru>
2022-10-22 14:52:22 +0300

    CompletableFuture [#361626]

---
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -0,0 +1,69 @@
+package ru.job4j.pools;
+
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class RolColSum {
+    public static class Sums {
+        private final int rowSum;
+        private final int colSum;
+        /* Getter and Setter */
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public Sums(int rowSum, int colSum) {
+            this.rowSum = rowSum;
+            this.colSum = colSum;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Sums sums)) {
+                return false;
+            }
+            return getRowSum() == sums.getRowSum() && getColSum() == sums.getColSum();
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getRowSum(), getColSum());
+        }
+    }
+
+    private static Sums calculateSumsForIndex(int index, int[][] matrix) {
+        int rowSum = 0;
+        int colSum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            rowSum += matrix[index][i];
+            colSum += matrix[i][index];
+        }
+        return new Sums(rowSum, colSum);
+    }
+
+    public static Sums[] sum(int[][] matrix) {
+        Sums[] sums = new Sums[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            sums[i] = calculateSumsForIndex(i, matrix);
+        }
+        return sums;
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        Sums[] sums = new Sums[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            int index = i;
+            sums[i] = CompletableFuture.supplyAsync(() -> calculateSumsForIndex(index, matrix)).get();
+        }
+        return sums;
+    }
+}
